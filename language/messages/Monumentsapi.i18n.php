a0a9b875ecff30e7a14deb63a41d7b4d23e3cff1
Krinkle <krinklemail@gmail.com>
2011-04-03 22:26:08 +0000

    adding all messages used so far with qqq
    git-svn-id: https://svn.wikimedia.org/svnroot/mediawiki/trunk/tools/ToolserverI18N@85291 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- language/messages/Monumentsapi.i18n.php
+++ language/messages/Monumentsapi.i18n.php
@@ -15,16 +15,20 @@ $url = '~erfgoed/api/';
 $messages['en'] = array(
 	'title' =>	'Monuments API', // Do not translate
 	'search-table-th-field' => 'Field',
+	'search-table-th-filter' => 'Filter',
+	'search-table-th-search' => 'Search',
 );
 
 /**
- * Documentation
+ * Documentation for TranslateWiki
  *
  * @author Krinkle
  */
 $messages['qqq'] = array(
 	'title' =>	"{{Notranslate}}\n\nThe title of the tool.",
-);
+	'search-table-th-field' => 'Table header for the Field-column in the search table. {{Identical|Field}}',
+	'search-table-th-filter' => '{{Identical|Filter}}',
+	'search-table-th-search' => '{{Identical|Search}}',
 
 /**
  * Nederlands
