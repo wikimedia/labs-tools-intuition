8db0a5d31ac77088d960e4423bb8ea0a36623c7b
Krinkle <krinklemail@gmail.com>
2011-04-05 23:17:47 +0000

    add general domain for messages used in most of my tools (header, footer, allwikis select dropdown etc.)
    git-svn-id: https://svn.wikimedia.org/svnroot/mediawiki/trunk/tools/ToolserverI18N@85488 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- language/Domains.php
+++ language/Domains.php
@@ -1,11 +1,12 @@
 <?php
 $domains = array(
-	'General'			=> 'General.i18n.php',
-	'Getwikiapi'		=> 'Getwikiapi.i18n.php',
-	'Jarry'				=> 'Jarry.i18n.php',
-	'Monumentsapi'		=> 'Monumentsapi.i18n.php',
-	'Orphantalk2'		=> 'Orphantalk2.i18n.php',
+	'General' => 'General.i18n.php',
+	'Getwikiapi' => 'Getwikiapi.i18n.php',
+	'Jarry' => 'Jarry.i18n.php',
+	'Krinkle' => 'Krinkle.i18n.php',
+	'Monumentsapi' => 'Monumentsapi.i18n.php',
+	'Orphantalk2' => 'Orphantalk2.i18n.php',
 	'Recentanonymousactivity' => 'Recentanonymousactivity.i18n.php',
-	'Svgtranslate'		=> 'Svgtranslate.i18n.php',
-	'Tsintuition'		=> 'Tsintuition.i18n.php',
+	'Svgtranslate' => 'Svgtranslate.i18n.php',
+	'Tsintuition' => 'Tsintuition.i18n.php',
 );
---
+++ language/messages/Krinkle.i18n.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Interface messages for Krinkle's general tool interface.
+ *
+ * @toolowner krinkle
+ */
+
+$url = '~krinkle/';
+
+$messages = array();
+
+/**
+ * English
+ *
+ * @author Krinkle
+ */
+$messages['en'] = array(
+	'alws-selectwiki' => '(select wiki)',
+	'alws-group-mustused' => 'Most used wikis',
+	'alws-group-allaz' => 'All wikis alphabetically',
+);
+
+/**
+ * Documentation
+ *
+ * @author Krinkle
+ */
+$messages['qqq'] = array(
+	'alws-selectwiki' => 'This is the initial value for the dropdown menu with all wikis, of which one must be choosen.',
+	'alws-group-mustused' => 'This is the group label for group containing the most used wikis. In a dropdown menu with all wikis.',
+	'alws-group-allaz' => 'This is the group label for the group containg all wikis listed alphabetically. In a dropdown menu with all wikis.',
+);
+
+/**
+ * English
+ *
+ * @author Krinkle
+ */
+$messages['en'] = array(
+	'alws-selectwiki' => '(selecteer een wiki)',
+	'alws-group-mustused' => 'Meeste gebruikte wikis',
+	'alws-group-allaz' => 'Alle wikis op alfabetische volgorde',
+);
+
