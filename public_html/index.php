9f64011acefffdeed1465ac43550067e4888429e
Krinkle <krinklemail@gmail.com>
2011-04-10 09:51:05 +0000

    update graph to show weeks instead of days (same as on twn)
    git-svn-id: https://svn.wikimedia.org/svnroot/mediawiki/trunk/tools/ToolserverI18N@85754 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- public_html/index.php
+++ public_html/index.php
@@ -243,10 +243,12 @@ $toolSettings['tabs']['#tab-settingsform'] = _('tab-settings');
 $about = '<div id="tab-about">';
 	
 $about .= '<a href="http://translatewiki.net/wiki/Translating:Toolserver">'
-	.	'<img src="http://translatewiki.net/w/i.php?title=Special:TranslationStats&'
-	.	'graphit=1&width=520&height=400&group=tsint-0-all" width="520" height="400" alt="" class="floatRight" />'
+	.	'<img src="http://translatewiki.net/w/i.php?title=Special:TranslationStats'
+		.	'&graphit=1&preview=&count=edits&scale=weeks&days=30&width=600&height=400&group=tsint-0-all'
+	.	'" width="520" height="400" alt="" class="floatRight" />'
 	.	'</a>';
-$about .= 'Technical documentation: <a href="https://wiki.toolserver.org/view/Toolserver_Intuition">https://wiki.toolserver.org/view/Toolserver_Intuition</a>'
+$about .= 'Technical documentation: <a href="https://wiki.toolserver.org/view/Toolserver_Intuition">'
+	. 'https://wiki.toolserver.org/view/Toolserver_Intuition</a>'
 	. '<div class="tab-paragraph-head">' . _( 'usage' ) . '</div><ul>';
 foreach ( $I18N->getAllRegisteredDomains() as $domainKey => $domainFile ) {
 	$domainInfo = $I18N->getDomainInfo( $domainKey );
