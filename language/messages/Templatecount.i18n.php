99f243c79908ed075164b57501c2b131aabb4789
Raimond Spekking <raimond.spekking@gmail.com>
2011-07-13 19:14:28 +0000

    Localisation updates for ToolserverI18N messages from translatewiki.net (2011-07-13 19:0:00)
    git-svn-id: https://svn.wikimedia.org/svnroot/mediawiki/trunk/tools/ToolserverI18N@92084 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- language/messages/Templatecount.i18n.php
+++ language/messages/Templatecount.i18n.php
@@ -498,11 +498,12 @@ $messages['vi'] = array(
 
 /** Simplified Chinese (‪中文(简体)‬)
  * @author Hydra
+ * @author Xiaomingyan
  */
 $messages['zh-hans'] = array(
 	'enter-details' => '输入详细信息',
 	'language-label' => '语言',
-	'namespace-label' => '名字空间',
+	'namespace-label' => '名称空间',
 	'pagename-label' => '页面名称（不包括命名空间）',
 	'time-label' => '执行命令所需的时间',
 );
