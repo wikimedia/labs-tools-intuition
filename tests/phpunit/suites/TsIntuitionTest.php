3688b34272c645300fb0cf3cb8d9931dd2ccce93
Timo Tijhof <krinklemail@gmail.com>
2013-07-22 01:48:34 +0200

    test: Fix phpunit assertion arguments to be (expected, actual)
    Had them the wrong way around (surfaced in an error message
    when --- Expected and +++ Actual are flipped).

--- tests/phpunit/suites/TsIntuitionTest.php
+++ tests/phpunit/suites/TsIntuitionTest.php
@@ -19,16 +19,16 @@ class TsIntuitionTest extends PHPUnit_Framework_TestCase {
 		$i18n = new TsIntuition();
 
 		$this->assertEquals(
-			$i18n->msg( 'welcome' ),
 			'Welcome',
+			$i18n->msg( 'welcome' ),
 			'Defaults to "general"'
 		);
 
 		$i18n = new TsIntuition( 'tsintuition' );
 
 		$this->assertEquals(
-			$i18n->msg( 'tab-demo' ),
 			'Demo',
+			$i18n->msg( 'tab-demo' ),
 			'Domain key as string'
 		);
 
@@ -37,60 +37,60 @@ class TsIntuitionTest extends PHPUnit_Framework_TestCase {
 		) );
 
 		$this->assertEquals(
-			$i18n->msg( 'tab-demo' ),
 			'Demo',
+			$i18n->msg( 'tab-demo' ),
 			'Settings array with "domain" key'
 		);
 	}
 
 	public function testDateFormatted() {
 		$this->assertEquals(
-			$this->i18n->dateFormatted( '2001-01-15' ),
-			'January 15 2001'
+			'January 15 2001',
+			$this->i18n->dateFormatted( '2001-01-15' )
 		);
 	}
 
 	public function testMsg() {
-		$this->assertEquals(
-			$this->i18n->msg( 'welcome' ),
-			'Welcome',
-			'Getting a message'
-		);
-
-		$this->assertEquals(
-			$this->i18n->msg( 'r4nd0mstr1n9' ),
-			'[r4nd0mstr1n9]',
-			'Unknown key falls back to bracket-wrapped key'
-		);
-
-		$this->assertEquals(
-			$this->i18n->msg( 'toolversionstamp', array(
-				'variables' => array( '1.0', $this->i18n->dateFormatted( '2001-01-15' ) ),
-			) ),
-			'Version 1.0 (January 15 2001)',
-			'Variable replacement with dateFormatted'
-		);
-
 		$this->i18n->setMsgs( array(
 			'welcomeback' => 'Welcome back, $1! Would you like some $2?',
 			'basket' => 'The basket contains $1 {{PLURAL:$1|apple|apples}}.',
 		) );
 
 		$this->assertEquals(
-			$this->i18n->msg( 'welcomeback', array( 'variables' => array( 'John', 'coffee' ) ) ),
+			'Welcome',
+			$this->i18n->msg( 'welcome' ),
+			'Getting a message'
+		);
+
+		$this->assertEquals(
+			'[r4nd0mstr1n9]',
+			$this->i18n->msg( 'r4nd0mstr1n9' ),
+			'Unknown key falls back to bracket-wrapped key'
+		);
+
+		$this->assertEquals(
+			'Version 1.0 (January 15 2001)',
+			$this->i18n->msg( 'toolversionstamp', array(
+				'variables' => array( '1.0', $this->i18n->dateFormatted( '2001-01-15' ) ),
+			) ),
+			'Variable replacement with dateFormatted'
+		);
+
+		$this->assertEquals(
 			'Welcome back, John! Would you like some coffee?',
+			$this->i18n->msg( 'welcomeback', array( 'variables' => array( 'John', 'coffee' ) ) ),
 			'Replacing 2 veriables'
 		);
 
 		$this->assertEquals(
-			$this->i18n->msg( 'basket', array( 'variables' => array( '1' ), 'parsemag' => true ) ),
 			'The basket contains 1 apple.',
+			$this->i18n->msg( 'basket', array( 'variables' => array( '1' ), 'parsemag' => true ) ),
 			'Plural with 1'
 		);
 
 		$this->assertEquals(
-			$this->i18n->msg( 'basket', array( 'variables' => array( '7' ), 'parsemag' => true ) ),
 			'The basket contains 7 apples.',
+			$this->i18n->msg( 'basket', array( 'variables' => array( '7' ), 'parsemag' => true ) ),
 			'Plural with 7'
 		);
 	}
@@ -106,8 +106,8 @@ class TsIntuitionTest extends PHPUnit_Framework_TestCase {
 
 	public function testParentheses() {
 		$this->assertEquals(
-			$this->i18n->parentheses( 'hello' ),
-			'(Hello)'
+			'(Hello)',
+			$this->i18n->parentheses( 'hello' )
 		);
 	}
 
@@ -119,8 +119,8 @@ class TsIntuitionTest extends PHPUnit_Framework_TestCase {
 		) );
 
 		$this->assertEquals(
-			$i18n->msg( 'r4nd0mstr1n9' ),
 			'[r4nd0mstr1n9]',
+			$i18n->msg( 'r4nd0mstr1n9' ),
 			'Unknown key falls back to bracket-wrapped key'
 		);
 		$this->expectOutputString(
@@ -130,16 +130,16 @@ class TsIntuitionTest extends PHPUnit_Framework_TestCase {
 
 	public function testGetLangName() {
 		$this->assertEquals(
-			$this->i18n->getLangName(),
-			'English'
+			'English',
+			$this->i18n->getLangName()
 		);
 		$this->assertEquals(
-			$this->i18n->getLangName( 'fr' ),
-			'Français'
+			'Français',
+			$this->i18n->getLangName( 'fr' )
 		);
 		$this->assertEquals(
-			$this->i18n->getLangName( 'en' ),
-			'English'
+			'English',
+			$this->i18n->getLangName( 'en' )
 		);
 	}
 }
