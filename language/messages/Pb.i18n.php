ab78aa98b9695069a62cb5180908c6eb89466f1c
Robin Krahl <me@robin-krahl.de>
2013-12-03 17:14:17 +0100

    Added English translation for pb

--- language/messages/Pb.i18n.php
+++ language/messages/Pb.i18n.php
@@ -10,6 +10,78 @@ $url = 'https://tools.wmflabs.org/pb/';
 $messages = array();
 
 /**
+ * Englisch
+ *
+ * @author ireas
+ */
+$messages['en'] = array(
+    'about-pb-navi' => 'About',
+    'about-pb-tech' => 'Technical details',
+    'about-pb-tech-text' => '<p>We use <a href="https://en.wikipedia.org/wiki/Python_%28programming_language%29">Python</a> and <a href="https://en.wikipedia.org/wiki/MySQL">MySQL</a> for this interface. The source code is published under the terms of the <a href="http://www.gnu.org/licenses/lgpl.html" class="ext">GNU Lesser General Public License 3</a> (LGPL 3) and can be accessed <a href="https://github.com/tool-labs/pb">on GitHub</a>. Please add bug reports, feature requests or other comments <a href="https://github.com/tool-labs/pb/issues/new">to the GitHub issue tracker</a> or report it on the <a href="https://de.wikipedia.org/wiki/Wikipedia_Diskussion:Persönliche_Bekanntschaften">Wikipedia talk page</a>.</p>
+    <p>Current dumps of the PB database can be found <a href="/pb/sql-dumps">here</a>.</p>
+    <p>We use several icons created by the <a href="http://glyphicons.com/" title="Glyphicons homepage">Glyphicons</a> project.</p>',
+    'about-pb-text' => '<p>This project represents personal acquitances between Wikimedians.</p>
+    <p>The first implementation was done by Ulli Purwin (interface) and Euku (bot). In the beginning of 2011, Wiegels and Ireas followed Ulli doing the interface. By now, the project is not longer hosted on a private server but on Wikimedia Labs.</p>
+    <p>More information about this project can be found in the German Wikipedia on the page a href="https://de.wikipedia.org/wiki/Wikipedia:Persönliche_Bekanntschaften">Wikipedia:Persönliche Bekanntschaften</a>.</p>',
+    'about-pb-title' => 'About the project',
+    'confirmations-given' => 'Given confirmations',
+    'confirmations-taken' => 'Received confirmations',
+    'current-confirmations' => 'There are $1 confirmations in total, this is about <abbr title="$2">$3</abbr> confirmations per day.',
+    'current-figures' => 'Current figures',
+    'current-users' => 'At the moment, $1 users participate in the <em>Personal Acquitances</em> project.',
+    'footer' => '<a href="https://de.wikipedia.org/wiki/Wikipedia:Persönliche_Bekanntschaften" title="Wikipedia:Persönliche Bekanntschaften in the German Wikipedia">[[Wikipedia:Persönliche Bekanntschaften]]</a> on Tool Labs. <a href="$1" title="About Wikipedia:Persönliche Bekanntschaften">More information.</a>',
+    'home' => 'Home',
+    'home-welcome' => 'Welcome to the <em>Personal Acquitances</em> project(<em>Persönliche-Bekanntschaften</em> in German)!  More information about this project can be found <a href="https://de.wikipedia.org/wiki/Wikipedia:Persönliche_Bekanntschaften" title="Wikipedia:Persönliche Bekanntschaften in der deutschsprachigen Wikipedia">in the German Wikipedia</a>.',
+    'javascript-notice' => 'We recommend to enable JavaScript for this website to improve the user interface.',
+    'labs-name' => 'Wikimedia Tool Labs',
+    'participates-since' => 'Participates since',
+    'pb-name' => 'Wikipedia:Persönliche Bekanntschaften',
+    'powered-by-labs' => 'Powered by Wikimedia Tool Labs',
+    'recent-confirmation' => '$1: <a href="$2" title="Details for user $3">$3</a> → <a href="$4" title="Details for user $5">$5</a>',
+    'recent-confirmations' => 'Recent confirmations',
+    'user' => 'User',
+    'user-link-title' => 'Details for user $1',
+    'users-navi' => 'Users',
+    'users-title' => 'List of participators',
+    'users-overview' => 'These $1 users participate in the <em>Personal Acquitances</em> project:',
+    'verified-since' => 'Verified since',
+);
+
+/**
+ * Message documentation
+ *
+ * @author ireas
+ */
+$messages['qqq'] = array(
+    'about-pb-navi' => 'Navigation item ‘About PB’',
+    'about-pb-tech' => 'About section ‘Technical’',
+    'about-pb-tech-text' => 'Description of the technical details of the project (in <p> tag(s))',
+    'about-pb-text' => 'Description of the project (in <p> tag(s))',
+    'about-pb-title' => 'Title of the page ‘About PB’',
+    'confirmations-given' => 'Name for the confirmations done by a user',
+    'confirmations-taken' => 'Name for the confirmations received by a user',
+    'current-confirmations' => 'Label containing some figures about confirmations',
+    'current-figures' => 'Home section ‘Current figures’',
+    'current-users' => 'Label containing some figures about the participating users',
+    'footer' => 'Content of the footer',
+    'home' => 'Title of the home page',
+    'home-welcome' => 'Welcome label on the home page',
+    'javascript-notice' => 'Content of the noscript warning',
+    'labs-name' => 'Name of Wikimedia Tool Labs',
+    'participates-since' => 'Label for the date since when the user participates',
+    'pb-name' => 'Name of the WP:PB project',
+    'powered-by-labs' => 'Title for the ‘Powered by Labs’ logo',
+    'recent-confirmation' => 'Entry in the recent confirmation list on the home page',
+    'recent-confirmations' => 'Home section ‘Recent confirmations’',
+    'user' => 'Label for user names',
+    'user-link-title' => 'Title for a link to user details',
+    'users-navi' => 'Navgation item ‘Users’',
+    'users-title' => 'Title of the page ‘Users’',
+    'users-overview' => 'Introduction for the ‘Users’ page',
+    'verified-since' => 'Label for the date since when the user is verified',
+);
+
+/**
  * German
  *
  * @author ireas
@@ -45,39 +117,5 @@ $messages['de'] = array(
     'users-title' => 'Teilnehmerliste',
     'users-overview' => 'Die folgenden $1 Benutzer nehmen zurzeit am Projekt <em>Persönliche Bekanntschaften teil:',
     'verified-since' => 'Bestätigt seit',
-);
-
-/**
- * Message documentation
- *
- * @author ireas
- */
-$messages['qqq'] = array(
-    'about-pb-navi' => 'Navigation item ‘About PB’',
-    'about-pb-tech' => 'About section ‘Technical’',
-    'about-pb-tech-text' => 'Description of the technical details of the project (in <p> tag(s))',
-    'about-pb-text' => 'Description of the project (in <p> tag(s))',
-    'about-pb-title' => 'Title of the page ‘About PB’',
-    'confirmations-given' => 'Name for the confirmations done by a user',
-    'confirmations-taken' => 'Name for the confirmations received by a user',
-    'current-confirmations' => 'Label containing some figures about confirmations',
-    'current-figures' => 'Home section ‘Current figures’',
-    'current-users' => 'Label containing some figures about the participating users',
-    'footer' => 'Content of the footer',
-    'home' => 'Title of the home page',
-    'home-welcome' => 'Welcome label on the home page',
-    'javascript-notice' => 'Content of the noscript warning',
-    'labs-name' => 'Name of Wikimedia Tool Labs',
-    'participates-since' => 'Label for the date since when the user participates',
-    'pb-name' => 'Name of the WP:PB project',
-    'powered-by-labs' => 'Title for the ‘Powered by Labs’ logo',
-    'recent-confirmation' => 'Entry in the recent confirmation list on the home page',
-    'recent-confirmations' => 'Home section ‘Recent confirmations’',
-    'user' => 'Label for user names',
-    'user-link-title' => 'Title for a link to user details',
-    'users-navi' => 'Navgation item ‘Users’',
-    'users-title' => 'Title of the page ‘Users’',
-    'users-overview' => 'Introduction for the ‘Users’ page',
-    'verified-since' => 'Label for the date since when the user is verified',
 );
 
