da4d3c8ae97414cb37fc0630c5295661f34163a9
Timo Tijhof <krinklemail@gmail.com>
2014-02-27 14:57:40 +0100

    wpAvailableLanguages: Pass phpcs

--- public_html/wpAvailableLanguages.js.php
+++ public_html/wpAvailableLanguages.js.php
@@ -8,38 +8,49 @@
  */
 
 // Configuration
-date_default_timezone_set('UTC');
-ini_set('user_agent', 'wpAvailableLanguages.js.php/v1.0.1 (rev 2014-02-26; Author: github.com/Krinkle)');
+date_default_timezone_set( 'UTC' );
+ini_set(
+	'user_agent',
+	'wpAvailableLanguages.js.php/v1.0.1 (rev 2014-02-26; Author: github.com/Krinkle)'
+);
 
 /**
  * Polyfill for json_encode JSON_UNESCAPED_UNICODE (new in PHP 5.4.0) for PHP 5.3
  */
-function kf_json_encode($arr) {
-	array_walk_recursive($arr, function (&$item, $key) {
-		if (is_string($item)) {
-			$item = mb_encode_numericentity($item, array (0x80, 0xffff, 0, 0xffff), 'UTF-8');
+function kfJsonEncode( $arr ) {
+	array_walk_recursive( $arr, function ( &$item, $key ) {
+		if ( is_string( $item ) ) {
+			$item = mb_encode_numericentity( $item, array( 0x80, 0xffff, 0, 0xffff ), 'UTF-8' );
 		}
-	});
-    return mb_decode_numericentity(json_encode($arr), array (0x80, 0xffff, 0, 0xffff), 'UTF-8');
+	} );
+	return mb_decode_numericentity( json_encode( $arr), array( 0x80, 0xffff, 0, 0xffff ), 'UTF-8' );
 }
 
 // Header
-header('Content-Type: text/javascript; charset=utf-8');
+header( 'Content-Type: text/javascript; charset=utf-8' );
 
 // Download
-$result = file_get_contents('http://commons.wikimedia.org/w/api.php?format=json&action=query&meta=siteinfo&siprop=languages');
+$result = file_get_contents( 'http://commons.wikimedia.org/w/api.php?' . http_build_query( array(
+	'format' => 'json',
+	'action' => 'query',
+	'meta' => 'siteinfo',
+	'siprop' => 'languages'
+) ) );
 
 // Convert to more compact format
-$result = json_decode($result, /* $assoc = */ true);
+$result = json_decode( $result, /* $assoc = */ true );
 $result = $result['query']['languages'];// [0] => array('code' => 'af', '*' => 'Afrikaans')
 $json_return = array();
-foreach ($result as $lang) {
-        $json_return[$lang['code']] = $lang['*'];
+foreach ( $result as $lang ) {
+	$json_return[ $lang['code'] ] = $lang['*'];
 }
 
 // Spit it out
-echo '// Update from https://tools.wmflabs.org/intuition/wpAvailableLanguages.js.php - Last update: ' . date('r') . "\n";
-echo 'window.wpAvailableLanguages=';
-echo kf_json_encode( $json_return );
-echo ';';
+echo '// Update from https://tools.wmflabs.org/intuition/wpAvailableLanguages.js.php - Last update: '
+	. date( 'r' )
+	. "\n"
+	. 'window.wpAvailableLanguages='
+	. kfJsonEncode( $json_return )
+	. ';';
+
 die;
