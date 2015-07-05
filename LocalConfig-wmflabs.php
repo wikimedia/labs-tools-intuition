4c9520615cd0f98941065e921532585c39061389
Timo Tijhof <krinklemail@gmail.com>
2015-07-05 08:15:24 +0100

    Delete LocalConfig-wmflabs.php
    No longer needed.

--- LocalConfig-wmflabs.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * LocalConfig for ToolLabs instance at https://tools.wmflabs.org/intuition/.
- */
-
-function intuitionHookInit( $intuition ) {
-	$intuition->dashboardHome = '//tools.wmflabs.org/intuition/';
-}
