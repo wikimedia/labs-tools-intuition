360b084c69b69aeb969786dc75ad550ae83015a3
Timo Tijhof <krinklemail@gmail.com>
2016-07-09 01:57:10 +0100

    build: ⬆️  Update dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-jscs');
 	grunt.loadNpmTasks('grunt-jsonlint');
 
-
 	var fs = require('fs'),
 		path = require('path'),
 		msgDir = path.join(__dirname, 'language', 'messages'),
